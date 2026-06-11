d9efe3cbf06b1637fc9564dce32f82c5e6c55020
Illya Litkin <illya@cinedeck.com>
2026-06-11 14:20:57 +0200

    fix parsing sets with unsupported coding of registry designator

--- deps/libMXF/mxf/mxf_header_metadata.c
+++ deps/libMXF/mxf/mxf_header_metadata.c
@@ -872,8 +872,16 @@ int mxf_read_and_return_set(MXFFile *mxfFile, const mxfKey *key, uint64_t len,
 
     assert(headerMetadata->primerPack != NULL);
 
-    /* only read sets with known definitions */
-    if (mxf_find_set_def(headerMetadata->dataModel, key, &setDef))
+    /* only read sets with known definitions and supported Coding of Registry Designator (SMPTE ST 336:2017, table 5) */
+    if(key->octet6 != 0x53)
+    {
+        char key_str[KEY_STR_SIZE];
+        mxf_sprint_key(key_str, &key);
+        mxf_log_warn("Encountered set key %s with unsupported" 
+            "coding of registry designator (octet 6 is expected to be 0x53 only)"
+            LOG_LOC_FORMAT, key_str, LOG_LOC_PARAMS);
+    }
+    else if (mxf_find_set_def(headerMetadata->dataModel, key, &setDef))
     {
         CHK_ORET(create_empty_set(key, &newSet));
 
